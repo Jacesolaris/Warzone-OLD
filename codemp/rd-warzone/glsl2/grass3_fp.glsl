ff1e1444f63cafa6f764c356464b16804bbd615e
uq1 <uniquewon@gmail.com>
2016-03-12 12:33:32 +1100

    * Add Blinn Phong also to experimental grass glsl.

--- codemp/rd-warzone/glsl2/grass3_fp.glsl
+++ codemp/rd-warzone/glsl2/grass3_fp.glsl
@@ -15,6 +15,11 @@ uniform vec3		u_PrimaryLightAmbient;
 uniform float		u_PrimaryLightRadius;
 uniform vec4		u_ViewOrigin;
 
+uniform int			u_lightCount;
+uniform vec3		u_lightPositions2[16];
+uniform float		u_lightDistances[16];
+uniform vec3		u_lightColors[16];
+
 smooth in vec3		vWorldPos;
 
 #endif //defined(USE_PRIMARY_LIGHT) || defined(USE_PRIMARY_LIGHT_SPECULAR) || defined(USE_SHADOWMAP) 
@@ -41,18 +46,43 @@ void main()
 		vec4 PrimaryLightDir = vec4(u_PrimaryLightOrigin.xyz - (vWorldPos * u_PrimaryLightOrigin.w), u_PrimaryLightRadius * u_PrimaryLightRadius);
 	#endif //defined(USE_PRIMARY_LIGHT) || defined(USE_PRIMARY_LIGHT_SPECULAR) || defined(USE_SHADOWMAP) 
 
+	#if defined(USE_PRIMARY_LIGHT) || defined(USE_PRIMARY_LIGHT_SPECULAR) || defined(USE_SHADOWMAP) 
+		vec3 m_Normal = (vec3(clamp(length(1.0-diffuse.rgb) * 0.55, 0.0, 1.0)) * 2.0 - 1.0);
+		vec4 PrimaryLightDir = vec4(u_PrimaryLightOrigin.xyz - (vWorldPos * u_PrimaryLightOrigin.w), u_PrimaryLightRadius * u_PrimaryLightRadius);
+	#endif //defined(USE_PRIMARY_LIGHT) || defined(USE_PRIMARY_LIGHT_SPECULAR) || defined(USE_SHADOWMAP) 
+
 	#if defined(USE_PRIMARY_LIGHT) || defined(USE_PRIMARY_LIGHT_SPECULAR)
 		if (u_Local6.r > 0.0)
 		{
 			float lambertian2 = dot(PrimaryLightDir.xyz, m_Normal);
+			vec3 E = normalize(-(u_ViewOrigin.xyz - vWorldPos.xyz));
 
 			if(lambertian2 > 0.0)
 			{// this is blinn phong
-				vec3 E = normalize(-(u_ViewOrigin.xyz - vWorldPos.xyz));
 				vec3 halfDir2 = normalize(PrimaryLightDir.xyz + E);
 				float specAngle = max(dot(halfDir2, m_Normal), 0.0);
 				float spec2 = pow(specAngle, 16.0);
 				gl_FragColor.rgb -= vec3(spec2 * (1.0 - length(diffuse.rgb) / 3.0)) * gl_FragColor.rgb * u_PrimaryLightColor.rgb * 0.2 * u_Local5.b;
+			}
+
+			for (int li = 0; li < u_lightCount; li++)
+			{
+				vec3 lightDir = u_lightPositions2[li] - vWorldPos.xyz;
+				float lambertian3 = dot(lightDir.xyz, m_Normal);
+				float spec3 = 0.0;
+
+				if(lambertian3 > 0.0)
+				{
+					float lightStrength = clamp(1.0 - (length(lightDir) * (1.0 / u_lightDistances[li])), 0.0, 1.0) * 0.5;
+
+					if(lightStrength > 0.0)
+					{// this is blinn phong
+						vec3 halfDir3 = normalize(lightDir.xyz + E);
+						float specAngle3 = max(dot(halfDir3, m_Normal), 0.0);
+						spec3 = pow(specAngle3, 16.0);
+						gl_FragColor.rgb += vec3(spec3 * (1.0 - length(diffuse.rgb) / 3.0)) * u_lightColors[li].rgb * lightStrength * u_Local5.b;
+					}
+				}
 			}
 		}
 	#endif //defined(USE_PRIMARY_LIGHT) || defined(USE_PRIMARY_LIGHT_SPECULAR)
