f6795a24ac3f54b3a20c58e517c4a25c1a5a0a4f
uq1 <uniquewon@gmail.com>
2016-06-26 07:11:30 +1000

    * Oops. Forgot to disable that.

--- tools/wzmap/src/forest.c
+++ tools/wzmap/src/forest.c
@@ -100,9 +100,9 @@ void GenerateMapForest ( void )
 		Sys_Printf( "Adding %i trees to bsp.\n", FOLIAGE_NUM_POSITIONS );
 #endif
 		
-		for (i = 0; i < 32/*FOLIAGE_NUM_POSITIONS*/; i++)
+		for (i = 0; i < FOLIAGE_NUM_POSITIONS; i++)
 		{
-			printLabelledProgress("GenerateMapForest", i, 32/*FOLIAGE_NUM_POSITIONS*/);
+			printLabelledProgress("GenerateMapForest", i, FOLIAGE_NUM_POSITIONS);
 
 			const char		*classname, *value;
 			float			lightmapScale;
