95dbd6d623a66c862bc68b16c6c92f49da3ce777
Stoiss <Stoiss3@hotmail.com>
2016-12-18 08:34:00 +0100

    remove a old saber call system call

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -772,9 +772,8 @@ struct gclient_s {
 
 	int			g2LastSurfaceHit; //index of surface hit during the most recent ghoul2 collision performed on this client.
 	int			g2LastSurfaceTime; //time when the surface index was set (to make sure it's up to date)
-	//[BUGFIX12]
-	int			g2LastSurfaceModel; //the index of the model on the ghoul2 that was hit during the lastest hit.
-	//[BUGFIX12]
+	
+
 	int			corrTime;
 
 	vec3_t		lastHeadAngles;
