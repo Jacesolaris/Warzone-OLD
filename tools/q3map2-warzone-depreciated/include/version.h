036e1fddba36f8e7cb9468194c04d24477655a43
uq1 <uniquewon@gmail.com>
2017-09-13 00:44:42 +1000

    * Removed WzMap from repo and move it to its own repo at https://github.com/uq1/WzMap * Remove my older q3map2 code as well.

--- tools/q3map2-warzone-depreciated/include/version.h
+++
@@ -1,4 +0,0 @@
-// generated header, see makeversion.py
-#define RADIANT_VERSION "1.6.4"
-#define RADIANT_MINOR_VERSION "4"
-#define RADIANT_MAJOR_VERSION "6"
--- tools/wzmap/src/trace_kernel.cl
+++
@@ -1,5 +0,0 @@
-/* The OpenCL kernels for q3map2's raytracing functions found in the light compile stage */ 
-
-__kernel void another_placeholder()
-{
-}
