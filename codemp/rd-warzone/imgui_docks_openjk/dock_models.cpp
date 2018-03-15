64520143230605e982841a7bd20461b61b79bd23
kungfooman <lama12345@gmail.com>
2018-03-15 16:21:39 +0100

    add dock_mdxa

--- codemp/rd-warzone/imgui_docks_openjk/dock_models.cpp
+++ codemp/rd-warzone/imgui_docks_openjk/dock_models.cpp
@@ -2,6 +2,7 @@
 
 #include "../imgui_docks/dock_console.h"
 #include "../imgui_docks_openjk/dock_mdxm.h"
+#include "../imgui_docks_openjk/dock_mdxa.h"
 //#include <renderergl2/tr_model_kung.h>
 #include "rd-warzone/tr_glsl.h"
 #include "../tr_local.h"
@@ -31,6 +32,38 @@ void imgui_mdxm(model_t *mod) {
 		new DockMDXM(mod);
 	}
 }
+void imgui_mdxa(model_t *mod) {
+	mdxaHeader_t *header = mod->data.gla;
+
+	//int			ident;				// 	"IDP3" = MD3, "RDM5" = MDR, "2LGA"(GL2 Anim) = MDXA
+	//int			version;			// 1,2,3 etc as per format revision
+	//char		name[MAX_QPATH];	// GLA name (eg "skeletons/marine")	// note: extension missing
+	//float		fScale;				// will be zero if build before this field was defined, else scale it was built with
+	//// frames and bones are shared by all levels of detail
+	//int			numFrames;
+	//int			ofsFrames;			// points at mdxaFrame_t array
+	//int			numBones;			// (no offset to these since they're inside the frames array)
+	//int			ofsCompBonePool;	// offset to global compressed-bone pool that all frames use
+	//int			ofsSkel;			// offset to mdxaSkel_t info
+	//int			ofsEnd;				// EOF, which of course gives overall file size
+
+	ImGui::Text("ident=%d version=%d name=%s fScale=%f numFrames=%d ofsFrames=%d numBones=%d ofsCompBonePool=%d ofsSkel=%d ofsEnd=%d",
+		header->ident,
+		header->version,
+		header->name,
+		header->fScale,
+		header->numFrames,
+		header->ofsFrames,
+		header->numBones,
+		header->ofsCompBonePool,
+		header->ofsSkel,
+		header->ofsEnd
+	);
+
+	if (ImGui::Button("Open")) {
+		new DockMDXA(mod);
+	}
+}
 
 void DockModels::imgui() {
 	for (int i=0; i<tr.numModels; i++) {
@@ -41,6 +74,7 @@ void DockModels::imgui() {
 			ImGui::PushID(model);
 			switch (model->type) {
 				case MOD_MDXM: imgui_mdxm(model); break;
+				case MOD_MDXA: imgui_mdxa(model); break;
 			}
 			ImGui::PopID();
 		}
